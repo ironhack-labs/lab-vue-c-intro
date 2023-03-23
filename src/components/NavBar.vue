2940512d3f2db9f52769f590a33457f9410ca578
Gissela Sobenes <gisselasobenes@gmail.com>
2023-03-23 22:09:30 +0100

    cleaning default value

---
+++ src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+        name: 'NavBar'
+    }
+</script>
+
+<template>
+  <h1>Hola</h1>
+</template>
+
+<style scoped>
+                                                                                    
+</style>
