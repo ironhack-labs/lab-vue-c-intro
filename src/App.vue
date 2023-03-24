e8264052f49271614988db746192c0dced28b067
Belen-Rodriguez <122569690+Belen-Rodriguez@users.noreply.github.com>
2023-03-24 17:06:24 +0100

    lab started

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <navBar />
+  <h1>prueba h1</h1>
+</template>
+
+<script>
+import navBar from './components/navBar.vue'
+
+export default {
+
+  name: "App",
+  components: {
+        navBar,
+      }
+}
+
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ src/components/navBar.vue
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<template>
+  <nav>
+    <img scr="" alt=""ironHackLogo>
+    <img scr="" alt=""ironHackLogo>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  background-color: aquamarine;
+
+}
+</style>
