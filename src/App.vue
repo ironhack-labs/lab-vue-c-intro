da68816976edab6fad635b4a97498aa367860ed6
edankap <edankap20@gmail.com>
2023-03-25 22:27:55 +0100

    navigarion bar done

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
-<script >
-export default {
-  name: "App",
-};
-</script>
-
 <template>
-<h1>Hola</h1>
+  <header>
+    <navBar />
+  </header>
+
+  <main>
+
+  </main>
+  <h1>Hola</h1>
 </template>
 
-<style scoped>
 
+<script >
+import navBar from './components/navBar.vue'
+export default {
+  name: "App",
+  components: {
+  navBar
+  }
+}
+</script>
+
+
+<style scoped>
+#app {
+
+}
+header {
+  margin: 20px 30px;
+}
 </style>
---
+++ src/components/navBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <img class = "ironHackLogo" src="../Images/Ironhack_logo.svg" alt="ironHackLogo">
+        <img class = "burguerLogo" src="../Images/Burger_menu.svg" alt="burguerMenuLogo">
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style scoped>
+    div {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 100%;
+    }
+    .ironHackLogo {
+        width: 35px;
+        height: 38px;
+    }
+    .burguerLogo {
+        width: 22px;
+        height: 20px;
+        width: auto;
+    }
+</style>
