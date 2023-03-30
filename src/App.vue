a761219526ec4a099af5773d05dda3f7afcde707
Rafaela Ridolphi <rafaela-ridolphi@hotmail.com>
2023-03-30 21:57:35 +0200

    updated

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <Main />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+import Main from './components/Main.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Main
+  }
+}
+</script>
+
+<style>
+  #app {
+    width: 1400px;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,57 @@
+<template>
+    <main>
+        <section>
+            <div class="vuelogo">                
+            </div>
+        </section>
+        <div>
+            <h1>Welcome to Vue.Js</h1>
+            <p>Become a coding ninja with the fastest-growing JavaScript framework</p>
+            <button></button>
+        </div>
+        <section>
+            <div class="awesome-element">Awesome!</div>
+        </section>
+
+    </main>
+
+</template>
+
+<style scoped>
+
+main {
+    font-family: inter;
+    font-weight: bold;
+}
+
+.vuelogo {
+    background-image: url("src/assets/logo.svg");
+    background-repeat: no-repeat;
+    width: 129px;
+    height: 129px;
+    margin-top: 28px;
+    margin-bottom: 50px;
+}
+
+h1 {
+    font-size: 50px;
+    color: #2B2F30;
+    line-height: 95px;
+    text-align: center;
+    margin-bottom: 24px;
+}
+
+p {
+    font-size: 14px;
+    color: #000000;
+    text-align: center;
+    letter-spacing: -0.02em;
+    margin-bottom: 27px;
+}
+
+.aewsome-element {
+    font-size: 14px;
+    text-align: center;
+}
+
+</style>
