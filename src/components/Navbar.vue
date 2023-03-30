e8f93ae9e540c7a5641ba38f7340f7e1991b8f21
Rafaela Ridolphi <rafaela-ridolphi@hotmail.com>
2023-03-30 21:53:29 +0200

    Updated

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<script setup>
+    export default {
+        name: "Navbar",
+
+    }
+</script>
+
+<template>
+    <nav class="navbar">
+        <div class="logo"></div>
+        <div class="menu"></div>
+    </nav>
+    </template>
+   
+    <style scoped>
+    
+    .navbar {
+        display: flex;
+        justify-content: space-between;
+    }
+    .logo {
+        background-image: url('src/assets/Ironhack_logo.png');
+        background-repeat: no-repeat;
+        width: 35px;
+        height: 38px;
+    }
+
+    .menu {
+        background-image: url("src/assets/Burger_menu (1).png");
+        background-repeat: no-repeat;
+        width: 22px;
+        height: 20px;
+    }
+    
+    </style>
