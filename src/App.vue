9bebe4da77318ce1185861d580c15c180a50c4b9
Rafaela Ridolphi <rafaela-ridolphi@hotmail.com>
2023-04-01 15:31:25 +0200

    Add .gitignore and ignore node_modules

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 <script>
 import Navbar from './components/Navbar.vue';
 import Main from './components/Main.vue';
-import Footer from './components/Footer.vue';
+import Footer from './components/fogo.vue';
 
 export default {
   name: 'App',
