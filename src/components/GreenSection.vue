d71456216648b595ee5ce325b4989f67fc7257f1
Pol Cadavid <polkadavid@gmail.com>
2023-04-02 19:09:54 +0200

    done

---
+++ src/components/GreenSection.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="greenSection">
+    <CardInGreenSection :image="card1.image" :greenTitle="card1.title" :greenText="card1.text" />
+    <CardInGreenSection :image="card2.image" :greenTitle="card2.title" :greenText="card2.text" />
+    <CardInGreenSection :image="card3.image" :greenTitle="card3.title" :greenText="card3.text" />
+  </div>
+</template>
+
+<script>
+import CardInGreenSection from './CardInGreenSection.vue'
+
+export default {
+  name: 'GreenSection',
+  components: {
+    CardInGreenSection
+  },
+  data() {
+    return {
+      card1: {
+        image: '/src/assets/USP-1.png',
+        title: 'Performant',
+        text: 'Truly reactive, compiled optimized rendering system that rarely requires manual optimization.'
+      },
+      card2: {
+        image: '/src/assets/USP-2.png',
+        title: 'Approachable',
+        text: 'Builds on top of standard HTML, CSS and JavaScript with intuitive API and world-class documentation.'
+      },
+      card3: {
+        image: '/src/assets/USP-3.png',
+        title: 'Versatile',
+        text: 'A rich, incrementally adoptable ecosystem that scales between a library and a full-featured framework.'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.greenSection {
+  background-color: #39b983;
+  display: flex;
+  justify-content: center;
+  padding: 50px 0;
+  margin-bottom: 41px;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <img class="ironHackLogo" src="../images/Ironhack_logo.png" alt="" />
+    <img class="burgerMenuLogo" src="../images/Burger_menu.svg" alt="" />
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 20px 30px;
+}
+.ironHackLogo {
+  width: 35px;
+  height: 38px;
+}
+.burgerMenuLogo {
+  width: 22px;
+  height: 20px;
+}
+</style>
