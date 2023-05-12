81c1521039c09903a93480ea1f14235515d41d4b
mxlgrrr <106144699+mxlgrrr@users.noreply.github.com>
2023-05-12 23:40:13 +0200

    Commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import NavBar from './components/navBarComponent.vue'
+import PageHero from './components/pageHeroComponent.vue'
+import BenefitSection from './components/benefitComponent.vue'
+</script>
+
+<template>
+  <NavBar />
+  <PageHero />
+  <BenefitSection />
+</template>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800;900&display=swap');
+
+  body {
+    font-family: 'Inter', sans-serif;
+  }
+</style>
