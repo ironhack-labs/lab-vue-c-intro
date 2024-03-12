619bccfd58d5e1a36834edff1ac9fd96d82ca2d8
Andybetan <andres_betan@hotmail.com>
2024-03-12 20:58:00 +0100

    commit css main

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div>
     <nav>
-      <img src="/src/assets/images/Ironhack_logo.svg" alt="logo-ironhack">
-      <img src="/src/assets/images/Burger_menu.svg" alt="menu">
+      <img src="./assets/images/Ironhack_logo.svg" alt="logo-ironhack">
+      <img src="./assets/images/Burger_menu.svg" alt="menu">
     </nav>
+
     <div id="container-logo">
-      <img src="/src/assets/images/Vue_logo.svg" alt="container-logo">
+      <img src="./assets/images/Vue_logo.svg" alt="container-logo">
     </div>
+
     <div id="container-titles">
-      <h1 v-html="'Welcome to Vue.js'" class="custom-h1"></h1>
-      <h3 v-html="'Become a coding ninja with the fastest-<br>growing JavaScript framework'" class="custom-h3"></h3>
-    </div>
-    <div>
+      <h1 class="custom-h1">{{ title }}</h1>
+      <h3 class="custom-h3">{{ subtitle }}</h3>
       <button class="btn">Awesome!</button>
     </div>
+
   </div>
   <div class="images-container">
     <img src="/src/assets/images/USP-1.svg" alt="ima-1">
@@ -31,41 +32,21 @@
     <p>A rich, incrementally adoptable ecosystem that scales between a library and a full-featured framework.</p>
   </div>
 </template>
-
+<!-------------------------------- script -------------------------------->
+<script>
+export default {
+  data() {
+    return {
+      title: 'Welcome to Vue.js',
+      subtitle: 'Become a coding ninja with the fastest-growing JavaScript framework'
+    };
+  }
+}
+</script>
+<!--------------------------------- Style --------------------------------->
 <style scoped>
-#container-titles {
-  text-align: center;
-}
-
-.custom-h1 {
-  font-size: 50px;
-  line-height: 95px;
-  letter-spacing: -2%;
-  font-weight: 800;
-  color: var(--vt-c-grey-darkest);
-}
-
-.custom-h3 {
-  font-size: 14px;
-  line-height: 20px;
-  letter-spacing: -2%;
-  font-weight: 800;
-  margin-bottom: 40px;
-}
-
-.images-container {
-  height: 40vh;
-  width: 100%;
-  background-color: var(--vt-c-green);
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-}
-
-.images-container img {
-  margin-right: 60px;
-  margin-left: 60px;
-
-}
+@import '/src/assets/main.css';
+@import './base.css';
+@import './color.css';
+@import './button.css';
 </style>
